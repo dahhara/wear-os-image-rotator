48787c7e4bde896d712ea55415340cdb9552ed3e
dahhara <dahara.dev@gmail.com>
2023-08-28 22:31:02 +0900

    move views for componet class

--- app/src/main/java/com/example/imagerotator/presentation/MainActivity.kt
+++ app/src/main/java/com/example/imagerotator/presentation/MainActivity.kt
@@ -9,28 +9,11 @@ package com.example.imagerotator.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.MaterialTheme
-import com.example.imagerotator.R
-import java.util.Collections.rotate
+import com.example.imagerotator.presentation.components.RotateAnimation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,44 +32,6 @@ fun WearApp() {
     )
 }
 
-@Composable
-fun RotateImage(
-    modifier: Modifier = Modifier,
-    rotationDegrees: Float = 0f
-) {
-    Image(
-        modifier = modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background)
-            .rotate(rotationDegrees),
-        painter = painterResource(id = R.drawable.target_image),
-        contentDescription = "targetImage"
-    )
-}
-
-@Composable
-fun RotateAnimation(
-    modifier: Modifier = Modifier,
-    changeAmount: Float
-) {
-    var currentRotation by remember { mutableStateOf(0f) }
-    val rotation = remember { Animatable(currentRotation) }
-
-    LaunchedEffect(changeAmount != 0f) {
-        rotation.animateTo(
-            targetValue = changeAmount + 360f,
-            animationSpec = infiniteRepeatable(
-                animation = tween(
-                    durationMillis = 1000,
-                    easing = LinearEasing
-                ),
-                repeatMode = RepeatMode.Restart
-            )
-        )
-    }
-
-    RotateImage(modifier = modifier, rotationDegrees = rotation.value)
-}
 
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
---
+++ app/src/main/java/com/example/imagerotator/presentation/components/RotateImage.kt
@@ -0,0 +1,58 @@
+package com.example.imagerotator.presentation.components
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.wear.compose.material.MaterialTheme
+import com.example.imagerotator.R
+@Composable
+fun RotateImage(
+    modifier: Modifier = Modifier,
+    rotationDegrees: Float = 0f
+) {
+    Image(
+        modifier = modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+            .rotate(rotationDegrees),
+        painter = painterResource(id = R.drawable.target_image),
+        contentDescription = "targetImage"
+    )
+}
+
+@Composable
+fun RotateAnimation(
+    modifier: Modifier = Modifier,
+    changeAmount: Float
+) {
+    val currentRotation by remember { mutableStateOf(0f) }
+    val rotation = remember { Animatable(currentRotation) }
+
+    LaunchedEffect(changeAmount != 0f) {
+        rotation.animateTo(
+            targetValue = changeAmount + 360f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(
+                    durationMillis = 1000,
+                    easing = LinearEasing
+                ),
+                repeatMode = RepeatMode.Restart
+            )
+        )
+    }
+
+    RotateImage(modifier = modifier, rotationDegrees = rotation.value)
+}
